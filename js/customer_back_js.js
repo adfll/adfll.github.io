011dee3b62ecec87e67319cf7ce97d9f1e9ead1b
adfll <152595196+adfll@users.noreply.github.com>
2023-12-01 17:13:48 +0900

    Update customer_back_js.js

--- js/customer_back_js.js
+++ js/customer_back_js.js
@@ -3,5 +3,5 @@ let back = document.getElementById("back");
 
 // 防災グッズのボタンをクリック
 back.addEventListener("click", () => {
-   location.href = "index.html";
+   location.href = "customer_index.html";
  });
